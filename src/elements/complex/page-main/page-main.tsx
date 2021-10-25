62caff1ade304ffab0b02c825a31e553ae63b85c
mounirhnf <mounirhnf@outlook.fr>
2021-10-25 19:23:42 +0100

    Integrate the loading feedback into the app

--- src/elements/complex/page-main/page-main.tsx
+++ src/elements/complex/page-main/page-main.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Store} from 'store/types';
+
+import {useSelector} from 'react-redux';
+
+import LoadingFeedback from 'elements/simple/loading-feedback';
+
+import buildClass from 'utility/build-class';
+
+import cls from './page-main.module.scss';
 
 //------------------------------------------------------------------------------
 
 const Main: React.FC = () => {
-  return <h1>Photographer</h1>;
+  const screenType = useSelector((store: Store.State) => store.screenType);
+
+  const mainClasses = buildClass(
+    cls['main'],
+    !screenType && cls['loading'],
+  );
+
+  return (
+    <>
+      {!screenType && <LoadingFeedback />}
+      <main className={mainClasses}>
+        <h1>Photographer</h1>
+      </main>
+    </>
+  );
 }
 
 //------------------------------------------------------------------------------
