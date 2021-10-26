58018c2fd4623d78acd1fe5d0ed45ec4dc251047
mounirhnf <mounirhnf@outlook.fr>
2021-10-26 12:15:18 +0100

    Add scroll navigation hook

---
+++ src/hooks/use-navigation.ts
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// This hooks provides functions that handle scroll navigation
+//------------------------------------------------------------------------------
+
+import {useDispatch} from 'react-redux';
+
+import {seekGallery} from 'store/actions';
+
+//------------------------------------------------------------------------------
+
+export default function useNavigation() {
+  const dispatch = useDispatch();
+
+  return {
+    goToGallery: () => dispatch(seekGallery(true)),
+    resetScroll: () => window.scrollTo({top: 0, behavior: 'smooth'}),
+  };
+}
