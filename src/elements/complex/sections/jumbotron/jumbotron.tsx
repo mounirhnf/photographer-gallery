a32a79b69f5c94bda98400303a5491add0303de7
mounirhnf <mounirhnf@outlook.fr>
2021-10-26 17:40:42 +0100

    Add jumbotron modal and integrate the about section into it

--- src/elements/complex/sections/jumbotron/jumbotron.tsx
+++ src/elements/complex/sections/jumbotron/jumbotron.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Store} from 'store/types';
 
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {setCurrentContext} from 'store/actions';
 
 import useNavigation from 'hooks/use-navigation';
 
+import Portal from 'elements/simple/portal';
+import Modal from 'elements/complex/modal';
+import About from 'elements/complex/sections/about';
 import Button from 'elements/complex/button';
 import SocialIcon from 'elements/complex/social-icon';
 
@@ -23,14 +28,23 @@ import cls from './jumbotron.module.scss';
 
 const Jumborton: React.FC = () => {
   const screenType = useSelector((store: Store.State) => store.screenType);
+  const context = useSelector((store: Store.State) => store.currentContext);
+  const noOverflow = useSelector((store: Store.State) => store.noOverflow);
+  const dispatch = useDispatch();
+
   const coverRef = React.useRef<HTMLImageElement | null>(null);
 
   const {goToGallery} = useNavigation();
 
   const classes = buildClass(
     cls['root'],
+    noOverflow && cls['offset'],
     screenType === 'xs' && cls['root-xs'],
   );
+
+  const openModal = (id: 'about' | 'contact') => {
+    dispatch(setCurrentContext(`no-click-away_jumbotron-${id}`))
+  };
 
   React.useEffect(() => {
     function slider(this: Window) {
@@ -46,48 +60,67 @@ const Jumborton: React.FC = () => {
   }, []);
 
   return (
-    <header className={classes}>
-      <img
-        className={cls['avatar']}
-        src={`${env.host}/img/ui/avatar.jpg`}
-        alt='avatar'
-      />
-      <h1 className={cls['name']}>JOHN DOE</h1>
-      <h2 className={cls['proffession']}>Professional Photographer</h2>
-      <ul className={cls['socials']}>
-        {socialLinks.map(({id, url}) => {
-          return (
-            <li key={id} className={cls['social-w']}>
-              <a  title={id}
-                  className={cls['social']}
-                  href={url}
-                  target='_blank'
-                  rel='noreferrer noopener'>
-                <SocialIcon className={cls['social-icon']} name={id} />
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <Button className={cls['button']} icon={AboutIcon}>
-        More about me
-      </Button>
-      <Button className={cls['button']} icon={ContactIcon} outlined>
-        Contact me
-      </Button>
-      <div
-        title='Scroll Down'
-        className={cls['mouse']}
-        onClick={goToGallery}
-      />
-      <img
-        ref={coverRef}
-        className={cls['cover']}
-        src={`${env.host}/img/ui/cover.jpg`}
-        alt='cover'
-      />
-      <div className={cls['overlay']} />
-    </header>
+    <>
+      <header className={classes}>
+        <img
+          className={cls['avatar']}
+          src={`${env.host}/img/ui/avatar.jpg`}
+          alt='avatar'
+        />
+        <h1 className={cls['name']}>JOHN DOE</h1>
+        <h2 className={cls['proffession']}>Professional Photographer</h2>
+        <ul className={cls['socials']}>
+          {socialLinks.map(({id, url}) => {
+            return (
+              <li key={id} className={cls['social-w']}>
+                <a  title={id}
+                    className={cls['social']}
+                    href={url}
+                    target='_blank'
+                    rel='noreferrer noopener'>
+                  <SocialIcon className={cls['social-icon']} name={id} />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button
+            className={cls['button']}
+            icon={AboutIcon}
+            onClick={() => openModal('about')}>
+          More about me
+        </Button>
+        <Button
+            className={cls['button']}
+            icon={ContactIcon}
+            outlined
+            onClick={() => openModal('contact')}>
+          Contact me
+        </Button>
+        <div
+          title='Scroll Down'
+          className={cls['mouse']}
+          onClick={goToGallery}
+        />
+        <img
+          ref={coverRef}
+          className={cls['cover']}
+          src={`${env.host}/img/ui/cover.jpg`}
+          alt='cover'
+        />
+        <div className={cls['overlay']} />
+      </header>
+      {context && context.includes('jumbotron') && <Portal>
+        <Modal
+            title={modalTitles[context]}
+            onClose={() => dispatch(setCurrentContext(null))}>
+          {context === 'no-click-away_jumbotron-about' ?
+            <About /> :
+            <h1>Contact me</h1>
+          }
+        </Modal>
+      </Portal>}
+    </>
   );
 }
 
@@ -99,6 +132,11 @@ const socialLinks = [
   {id: 'instagram', url: 'https://www.instagram.com/'},
   {id: 'facebook', url: 'https://www.facebook.com/'},
 ];
+
+const modalTitles: {[key: string]: string} = {
+  'no-click-away_jumbotron-about': 'about me',
+  'no-click-away_jumbotron-contact': 'contact me',
+};
 
 //------------------------------------------------------------------------------
 
