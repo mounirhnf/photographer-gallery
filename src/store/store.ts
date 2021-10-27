ef1b0ce234a47fffc6dbeb6b260c578b3143ab08
mounirhnf <mounirhnf@outlook.fr>
2021-10-27 11:25:33 +0100

    Add redux thunk middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware, Middleware} from 'redux';
 import {createWrapper} from 'next-redux-wrapper';
+import reduxThunk from 'redux-thunk';
 
 import reducer from 'store/reducer';
 import logger from 'store/logger';
 
 //------------------------------------------------------------------------------
 
-const middlewares: Middleware[] = [];
+const middlewares: Middleware[] = [reduxThunk];
 
 // Add the logger middleware only in the development envirement
 process.env.NODE_ENV === 'development' && middlewares.push(logger);
